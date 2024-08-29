148ab763ac36e3b7f1e243ad5098e499b80a2387
Rushaine McBean <copasetickid@users.noreply.github.com>
2024-08-29 13:41:39 -0400

    Add nav partial and static files to be served for the FE for chapter 2.8 and 2.9
    - Adds in basic styles for the app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	//Use the mux.Hanlde() function to register the file server
+	// as the handler for all URL paths that start with "/static/".
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
