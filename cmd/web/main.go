d42ed354819db2270f51b202b7c6e68489c0de9b
Rushaine McBean <copasetickid@users.noreply.github.com>
2024-10-01 16:59:08 -0400

    Chapter 4.4: Creates the database connection cool
    -This setups allows the app to connect to the database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,18 +16,47 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
+	// Define a new command-line flag for the MySQL DSN string.
+	dsn := flag.String("dsn", "snippetweb:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	//Create a new MySQL connection pool into the separate function.
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
 	}
 
-	logger.Info("starting server", slog.Any("addr", ":4000"))
+	logger.Info("starting server", slog.Any("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// The openDB() function wraps sql.Open() and sql.Ping() methods to create a new connection pool.
+// and returns a pointer to the sql.DB connection pool.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	//Use the db.Ping() method to create a new connection pool.
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
